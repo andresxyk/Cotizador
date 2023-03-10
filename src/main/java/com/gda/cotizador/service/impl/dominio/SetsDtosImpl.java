7f6c3e6f0d1ce941a2ade5185f8f01b32fd498e4
Luis Gerardo <luis.riverar@gda.mx>
2023-03-10 09:43:35 -0600

    funcion para seteos de TOrdenSucursalCotizacionDto

--- src/main/java/com/gda/cotizador/service/impl/dominio/SetsDtosImpl.java
+++ src/main/java/com/gda/cotizador/service/impl/dominio/SetsDtosImpl.java
@@ -3,6 +3,7 @@ package com.gda.cotizador.service.impl.dominio;
 import java.math.BigDecimal;
 
 import com.gda.cotizador.dto.cotizasion.TOrdenExamenSucursalCotizacionDto;
+import com.gda.cotizador.dto.cotizasion.TOrdenSucursalCotizacionDto;
 import com.gda.cotizador.dto.general.GDAMenssageDto;
 
 public interface SetsDtosImpl {
@@ -35,10 +36,42 @@ public interface SetsDtosImpl {
      * @param Smotivocancelacion
      * @param Cperfil
      * @param Uvolumenexamen
-     * @return TOrdenExamenSucursalCotizacionDto
+     * @return TOrdenExamenSucursalCotizacionDto retorna el objeto con los valores setados
      */
     TOrdenExamenSucursalCotizacionDto setForTOrdenExamenSucursalCotizacionDto(Integer Kordensucursalcotizacion,Integer Cexamen, 
                     String Sexamen,BigDecimal Msubtotal,BigDecimal Mdescuentopromocion,BigDecimal Mdescuentoempresa,BigDecimal Mdescuentomedico, BigDecimal Mfacturaempresa,
                     BigDecimal Mpagopaciente,BigDecimal Miva,BigDecimal Mtotal, Integer Userid,Integer Cestadoregistro,Integer Cconvenio,
                     String Smotivocancelacion, Integer Cperfil,Integer Uvolumenexamen);
+
+     /**
+      * Funcion para setera los valores al dto TOrdenSucursalCotizacionDto
+      * @param Kordensucursal
+      * @param Cmarca
+      * @param Ssucursal
+      * @param Csucursalm
+      * @param Csucursaldestino
+      * @param Kpaciente
+      * @param Cmedico
+      * @param Smedico
+      * @param Msubtotalm
+      * @param Mdescuentopromocion
+      * @param Mdescuentoempresa
+      * @param Mdescuentomedico
+      * @param Mfacturaempresa
+      * @param Mpagopaciente
+      * @param Miva
+      * @param Piva
+      * @param Mtotal
+      * @param Userid
+      * @param Cestadoregistro
+      * @param Sobservacion
+      * @param Cconvenio
+      * @param Useridchange
+      * @param Kordensucursalcotizacion
+      * @return TOrdenSucursalCotizacionDto retorna el objeto con los valores setados
+      */
+     public TOrdenSucursalCotizacionDto setForTOrdenSucursalCotizacionDto(Integer Kordensucursal,Integer Cmarca,String Ssucursal,Integer Csucursalm,Integer Csucursaldestino,Integer Kpaciente,
+               Integer Cmedico,String Smedico,BigDecimal Msubtotalm,BigDecimal Mdescuentopromocion,BigDecimal Mdescuentoempresa,BigDecimal Mdescuentomedico,
+               BigDecimal Mfacturaempresa,BigDecimal Mpagopaciente,BigDecimal Miva,BigDecimal Piva,BigDecimal Mtotal,Integer Userid,Integer Cestadoregistro,
+               String Sobservacion,Integer Cconvenio,Integer Useridchange,Integer Kordensucursalcotizacion);
 }
